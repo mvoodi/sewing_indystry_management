4b1f98533be0b9c4c11700493ef60e13cad7db7e
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2025-03-15 16:12:39 +0600

    Initial commit

---
+++ src/main/java/kg/alatoo/sewing_industry_management/SewingIndustryManagementApplication.java
@@ -0,0 +1,13 @@
+package kg.alatoo.sewing_industry_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewingIndustryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SewingIndustryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/alatoo/sewing_industry_management/SewingIndustryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.alatoo.sewing_industry_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewingIndustryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
