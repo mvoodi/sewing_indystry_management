d960f5ca2bcf5162f49dcb8cd962af205ce11a4b
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2025-05-03 20:21:14 +0600

    add user, varificationToken entities in auth package

---
+++ src/main/java/kg/alatoo/sewing_industry_management/auth/model/VerificationToken.java
@@ -0,0 +1,28 @@
+package kg.alatoo.sewing_industry_management.auth.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+}
